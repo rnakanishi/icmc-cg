2957b35152ba14c34735f8d3b9817105b64babbb
Rafael Nakanishi <rafael.umino.nakanishi@gmail.com>
2019-01-30 14:27:11 -0200

    adding example file

---
+++ hello.cpp
@@ -0,0 +1,14 @@
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	
+	return 0;
+}
